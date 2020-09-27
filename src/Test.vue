d3ac1f1ce4875486600bba5c0447ef673fb94dc7
xoiopure <67890018+xoiopure@users.noreply.github.com>
2020-09-27 03:30:19 -0700

    Initialize vuedx

---
+++ src/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <child :foo="" :bar="1"></child>
+  </div>
+</template>
+
+<script>
+import * as _ from 'lodash'
+import { defineComponent } from 'vue'
+
+import Child from './Child.vue'
+
+export default defineComponent({
+  components: {
+    Component,
+    Child
+  },
+  props: ['foo', 'bar']
+})
+</script>
