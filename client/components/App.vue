031a945a7f17807c3eda072a80b8544d6159505e
xoiopure <67890018+xoiopure@users.noreply.github.com>
2020-09-27 03:30:16 -0700

    Initialize 1063

---
+++ client/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import Counter from './Counter.vue'
+import * as _ from 'lodash'
+
+export default Vue.extend({
+  components: {
+    Counter
+  }
+})
+</script>
